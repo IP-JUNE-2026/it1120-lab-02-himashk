4adc6fcecafff0b82b20d610b2e1c126513ff1ca
himashk <it26101882@my.sliit.lk>
2026-06-28 19:34:02 +0530

    Add files via upload

---
+++ Q2/IT26101882Lab2Q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class IT26101882Lab2Q2{
+	public static void main(String[]args){
+		
+		double side = 10;
+		double pi = 3.14;
+		
+		double perimeter = 4 * side;
+        double radius = perimeter / (2 * pi);
+		
+	System.out.println("Radius of the circular fence: "+radius);
+	}
+}
+	
