641c9dcec3442406d09a4dbd8f466c51b2a548ad
himashk <it26101882@my.sliit.lk>
2026-06-28 19:33:07 +0530

    Add files via upload

---
+++ Q1/IT26101882Lab2Q1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IT26101882Lab2Q1{
+	public static void main(String[]args){
+		
+		double perimeter = 100;
+		
+		double length = (perimeter / 2) * 4 / 7;
+		double width =  length * 3 / 4;
+		
+		System.out.println("Length of the fernce: "+length);
+		System.out.println("Width of the fernce: "+width);
+		
+	}
+}
+		
+		
